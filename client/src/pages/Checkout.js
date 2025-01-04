7d6dc553c9bddc54c1001d54258326f18b52002d
donatomarino <donato_8@icloud.com>
2025-01-04 15:53:58 +0100

    Arreglo Proyecto Tienda

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -15,6 +15,7 @@ export default function Checkout() {
     const removeCart = () => {
         // Remove los items del carrito actual
         localStorage.removeItem('cart');
+        localStorage.removeItem('buy');
     }
 
     const toUpper = (name) => {
@@ -23,14 +24,14 @@ export default function Checkout() {
 
     return (
         <div className="contenedor-checkout">
-            <h2>¡Compra Realizada con Éxito!</h2>
-            <h4>
+            <h2 className="cart-empty_title">¡Compra Realizada con Éxito!</h2>
+            <h4 className="cart-empty_p">
                 Pokemon comprados:
                 {pokemons.map((e) => (
                     <li>{toUpper(e.name)} x {e.quantity}</li>
                 ))}
             </h4>
-            <h4>Total gastado: {totalBuy}€</h4>
+            <h4 className="cart-empty_p">Total gastado: {totalBuy}€</h4>
             <Link to="/">
                 <Boton
                     clase="btn-compra"
